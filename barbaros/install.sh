cc89f830ab95fb98d67989061805b976c5f8272e
Batuhan <bc1428@vuhuv.com>
2025-01-11 22:25:04 +0300

    install.sh güncellendi.

--- barbaros/install.sh
+++ barbaros/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/barbaros.gpg] https://barbaros14.github.io/barbaros stable main' | sudo tee /etc/apt/sources.list.d/barbaros.list
+echo -e 'deb [arch=amd64 signed-by=/usr/share/keyrings/barbaros.gpg] https://barbaros14.github.io/barbaros stable main\ndeb [arch=amd64 signed-by=/usr/share/keyrings/barbaros.gpg] https://arslansah.com.tr/barbaros stable main' | sudo tee /etc/apt/sources.list.d/barbaros.list
 sudo wget -q -O- https://barbaros14.github.io/barbaros/dists/stable/Release.key | sudo apt-key --keyring /usr/share/keyrings/barbaros.gpg add -
